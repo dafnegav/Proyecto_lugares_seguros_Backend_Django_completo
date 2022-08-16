1cdb2c26cddfbb33a5d1d1e16a80ebee2059907e
Daf <ada_hack464@hackademy.mx>
2022-08-16 05:53:54 -0500

    actualizacion datos heroku

--- lugaresseguros/lugaresseguros/settings.py
+++ lugaresseguros/lugaresseguros/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         'NAME': 'd1t2s8biq53cko', #nombre de la base de datos que viene en heroku
         'USER': 'fvzgknnfefuldl', #usuario que viene en heroku
         'HOST': 'ec2-54-85-56-210.compute-1.amazonaws.com', #de heroku
-        'PASSWORD': 'ec2-54-85-56-210.compute-1.amazonaws.com',
-        'PORT': 5432
+        'PASSWORD': 'bc7d39044f995f2c552e9722db8a39166cc5e5081684a8880b8697b230a27b0f',
+        'PORT': '5432'
     }
 }
 
